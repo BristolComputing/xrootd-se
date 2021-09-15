81e5f05d6932e761990c04063eb0ab41545fe653
Luke Kreczko <lkreczko@googlemail.com>
2021-09-15 10:44:50 +0100

    improved parsing of Authfile: removed duplicates, shortened regex

--- etc/xrootd/extract_directories.py
+++ etc/xrootd/extract_directories.py
@@ -1,12 +1,12 @@
 from __future__ import print_function
 import re
 
-match = re.compile(r'(\/xrootd\/[a-zA-z0-9|\/]+)\s.*')
+match = re.compile(r'(\/xrootd\/[A-z|\/]+)\s.*')
 
 with open('Authfile') as f:
     content = f.read()
 
 results = match.findall(content)
 
-for result in sorted(results):
+for result in sorted(set(results)):
     print(result)
