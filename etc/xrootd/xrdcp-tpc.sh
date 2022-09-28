8fa68906aa37111c7196d6f89689a66d8037225d
Luke Kreczko <lkreczko@googlemail.com>
2022-09-28 10:32:35 +0100

    replaced TPC script with xrootd docs example

--- etc/xrootd/xrdcp-tpc.sh
+++ etc/xrootd/xrdcp-tpc.sh
@@ -4,8 +4,28 @@
 # /usr/bin/xrdcp --server -f $1 root://$XRDXROOTD_PROXY/$2
 
 # Get the last two variables as SRC and DST, all others are assumed as additional arguments
-OTHERARGS="${@:1:$#-2}"
-DSTFILE="${@:$#:1}"
-SRCFILE="${@:$#-1:1}"
+# OTHERARGS="${@:1:$#-2}"
+# DSTFILE="${@:$#:1}"
+# SRCFILE="${@:$#-1:1}"
 
-/usr/bin/xrdcp $OTHERARGS --server -f $SRCFILE root://$XRDXROOTD_PROXY/$DSTFILE
+# /usr/bin/xrdcp $OTHERARGS --server -f $SRCFILE root://$XRDXROOTD_PROXY/$DSTFILE
+
+set -- `getopt S: -S 1 $*`
+while [ $# -gt 0 ]
+do
+  case $1 in
+  -S)
+      ((nstreams=$2-1))
+      [ $nstreams -ge 1 ] && TCPstreamOpts="-S $nstreams"
+      shift 2
+      ;;
+  --)
+      shift
+      break
+      ;;
+  esac
+done
+
+src=$1
+dst=$2
+xrdcp --server $TCPstreamOpts -f $src root://$XRDXROOTD_PROXY/${dst}
