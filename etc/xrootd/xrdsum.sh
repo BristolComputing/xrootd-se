5c2762ce119601528459be02c890797ad9d0b84a
kreczko <lkreczko@googlemail.com>
2024-10-03 11:48:55 +0100

    xrdsum: switch to local checksums for now

--- etc/xrootd/xrdsum.sh
+++ etc/xrootd/xrdsum.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RESULT=$(xrdsum --verbose  --debug --log-file /var/log/xrootd/clustered/checksum.log get --file-system HTTP --store-result "$1")
+RESULT=$(xrdsum --verbose  --debug --log-file /var/log/xrootd/clustered/checksum.log get --file-system POSIX --store-result "$1")
 ECODE=$?
 
 # XRootD expects return on stdout - checksum followed by a new line
